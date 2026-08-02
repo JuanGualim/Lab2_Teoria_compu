5e11347c184e5ae061616436ab9acdcd63b68211
JuanGualim <gua24852@uvg.edu.gt>
2026-08-01 21:53:47 -0600

    Creación de clase para guardar el resultado y que otras clases puedan usarlo

--- Resultado.java
+++ Resultado.java
@@ -1,3 +1,36 @@
+import java.util.List;
+
 public class Resultado {
-    
+
+    // Indica si la expresión está balanceada o no
+    private boolean balanceada;
+
+    // Guarda todos los pasos realizados por el algoritmo
+    private List<String> pasos;
+
+    /**
+     * Constructor de la clase.
+     *
+     * @param balanceada true si la expresión está balanceada.
+     * @param pasos Lista con todos los pasos realizados.
+     */
+    public Resultado(boolean balanceada, List<String> pasos) {
+        this.balanceada = balanceada;
+        this.pasos = pasos;
+    }
+
+    /**
+     * Devuelve si la expresión está balanceada.
+     */
+    public boolean isBalanceada() {
+        return balanceada;
+    }
+
+    /**
+     * Devuelve la lista de pasos realizados.
+     */
+    public List<String> getPasos() {
+        return pasos;
+    }
+
 }
